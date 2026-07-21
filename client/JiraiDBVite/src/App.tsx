a17fae11066b1014e45778d5a554e3d1c41cff36
TheBestCupcake <cavaligrooby@gmail.com>
2026-07-21 14:48:59 +1200

    added navbar to app.tsx

--- client/JiraiDBVite/src/App.tsx
+++ client/JiraiDBVite/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesProvider from "./utils/routes";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className="container">
-          <RoutesProvider />
+          <div className="containerHeader">
+            <Navbar />
+          </div>
+          <div className="containerBody">
+            <RoutesProvider />
+          </div>
         </div>
       </BrowserRouter>
     </>
