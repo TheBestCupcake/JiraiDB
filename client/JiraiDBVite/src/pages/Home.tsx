8b6dc0db32c629994afddc07469e74feb527b554
TheBestCupcake <cavaligrooby@gmail.com>
2026-07-22 11:41:59 +1200

    css and html testing

--- client/JiraiDBVite/src/pages/Home.tsx
+++ client/JiraiDBVite/src/pages/Home.tsx
@@ -1,8 +1,23 @@
+import horizontal from "../assets/horizontalimgexmaple.jpg";
+import vertical from "../assets/verticalimgexample.jpg";
+import square from "../assets/squareimgexample.jpg";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section>
+      <div className="split">
+        <div className="split-left">
+          <img src={vertical} className="displayImage" />
+        </div>
+        <div className="split-right">
+          <h1>Home</h1>
+          <h2>Display</h2>
+          <p>Hi</p>
+          <h2>Waow</h2>
+          <p>Mwah</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
