04feb23401c4db09726c1e0849f0db1609ed3d5b
TheBestCupcake <cavaligrooby@gmail.com>
2026-07-22 11:41:44 +1200

    created image display page

---
+++ client/JiraiDBVite/src/pages/ItemDisplay.tsx
@@ -0,0 +1,13 @@
+function ItemDisplay() {
+  return;
+  <>
+    <section>
+      <div>
+        <img src="" />
+      </div>
+      <div></div>
+    </section>
+  </>;
+}
+
+export default ItemDisplay;
