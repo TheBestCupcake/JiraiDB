9a2dfc47912fe37f288f9f9add30848eb00fe445
TheBestCupcake <cavaligrooby@gmail.com>
2026-07-29 12:49:42 +1200

    init app.js

--- server/app.js
+++ server/app.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
   res.send('Root Route /');
 });
 
-app.listen(port, () => console.log(`Listening on ${port}.`));{port}.`));
+app.listen(port, () => console.log(`Listening on ${port}.`));
