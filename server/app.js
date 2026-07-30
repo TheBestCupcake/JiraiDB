183ea83a61e81fb42c14eeb937bfa86ed5766388
TheBestCupcake <cavaligrooby@gmail.com>
2026-07-31 11:23:06 +1200

    installed cors to connect frontend and backend

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const port = process.env.PORT || 3000
 
 //Routes
 const itemRoutes = require("./routes/routes");
 
+//Setup
 app.use(express.json());
+app.use(cors())
 
 //Routes
 app.use("/Clothes", itemRoutes);
