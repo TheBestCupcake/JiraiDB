e03f87226875ce2b03c28e9b715cbe47610d5c41
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-19 12:56:36 +1200

    Fixed login service

--- client/JiraiDBVite/src/utils/loginServices.tsx
+++ client/JiraiDBVite/src/utils/loginServices.tsx
@@ -1,10 +1,21 @@
 const apiURL = "http://localhost:3000";
 
-export const login = (username: string, password: string) => {
-  fetch(`${apiURL}/login`, {
+export const login = async (username: string, password: string) => {
+  const response = await fetch(`${apiURL}/login`, {
     method: "POST",
-    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
     credentials: "include",
-    body: JSON.stringify({ username: username, password: password }),
+    body: JSON.stringify({ username, password }),
   });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
 };
