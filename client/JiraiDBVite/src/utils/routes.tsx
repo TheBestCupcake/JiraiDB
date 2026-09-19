11907f77886abfcf46b329f5ca307e0416862a2d
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-19 13:45:21 +1200

    Added protection route to upload page.

--- client/JiraiDBVite/src/utils/routes.tsx
+++ client/JiraiDBVite/src/utils/routes.tsx
@@ -3,12 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Upload from "../pages/Upload";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 let routeVariables = [
   { path: "/", name: "Home", component: <Home /> },
   { path: "/Clothes/:id", name: ":id", component: <Home /> },
   { path: "/Login", name: "Login", component: <Login /> },
-  { path: "/Upload", name: "Upload", component: <Upload /> },
+  {
+    path: "/Upload",
+    name: "Upload",
+    component: (
+      <ProtectedRoute>
+        <Upload />
+      </ProtectedRoute>
+    ),
+  },
 ];
 
 function RoutesProvider() {
