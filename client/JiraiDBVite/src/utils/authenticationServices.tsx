099d1a9a786ca63dc2a804e3d326167ab2b1c95f
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-21 13:31:31 +1200

    Changed to account for new api path

--- client/JiraiDBVite/src/utils/authenticationServices.tsx
+++ client/JiraiDBVite/src/utils/authenticationServices.tsx
@@ -1,7 +1,7 @@
 const apiURL = "http://localhost:3000";
 
 export const isAuthenticated = async () => {
-  const response = await fetch(`${apiURL}/restricted`, {
+  const response = await fetch(`${apiURL}/Clothes/upload`, {
     credentials: "include",
   });
 
