4cef657c96a330b21bd00b31adae9eca0cea6995
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-21 14:06:05 +1200

    added restrict check route. uses restrict middleware from services to check auth status.

--- server/routes/authenticationRoutes.js
+++ server/routes/authenticationRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { login, logout } = require('../controllers/authenticationcontroller');
+const { login, logout, isAuthed } = require('../controllers/authenticationcontroller');
+const { restrict } = require('../utils/authServices');
 const router = express.Router();
 
 //router.get(Auth/'path', response value); can also do post and other requests instead of get.
+router.post('/', restrict, isAuthed); //restrict used to check auth status first.
 router.post('/login', login);
 router.post('/logout', logout);
 
