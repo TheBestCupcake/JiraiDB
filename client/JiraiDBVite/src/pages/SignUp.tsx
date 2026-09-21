75d812cb094f795b19ebb471cbee4a10d366bfda
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-21 13:40:19 +1200

    temp form

--- client/JiraiDBVite/src/pages/SignUp.tsx
+++ client/JiraiDBVite/src/pages/SignUp.tsx
@@ -1,8 +1,16 @@
 function SignUp() {
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+  }
   return (
     <>
       <h1>Sign Up</h1>
-      <form method="post">
+      <form method="post" onSubmit={handleFormSubmit}>
         <p>
           <label>Username:</label>
           <input type="text" name="username" id="username" />
@@ -12,7 +20,7 @@ function SignUp() {
           <input type="password" name="password" id="password" />
         </p>
         <p>
-          <input type="submit" value="Login" />
+          <input type="submit" value="SignUp" />
         </p>
       </form>
     </>
