f16affc06e0e378074f94fef5c0a85d12fbdb2a3
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-21 13:32:06 +1200

    Added more login services.

--- client/JiraiDBVite/src/utils/loginServices.tsx
+++ client/JiraiDBVite/src/utils/loginServices.tsx
@@ -1,7 +1,31 @@
 const apiURL = "http://localhost:3000";
 
 export const login = async (username: string, password: string) => {
-  const response = await fetch(`${apiURL}/login`, {
+  const response = await fetch(`${apiURL}/Auth/login`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ username, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  if (response.ok) {
+    console.log(data.message);
+  }
+
+  return data;
+};
+
+export const signUp = async (username: string, password: string) => {
+  const response = await fetch(`${apiURL}/Auth/signup`, {
     method: "POST",
     headers: {
       Accept: "application/json",
