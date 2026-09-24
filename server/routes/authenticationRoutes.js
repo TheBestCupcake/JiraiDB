47c7a2e337a9c0eaaf67854b400dcd2e988bc02a
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-24 14:50:10 +1200

    added register route

--- server/routes/authenticationRoutes.js
+++ server/routes/authenticationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login, logout, isAuthed } = require('../controllers/authenticationcontroller');
+const { login, logout, isAuthed, register } = require('../controllers/authenticationcontroller');
 const { restrict } = require('../utils/authServices');
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', restrict, isAuthed); //restrict used to check auth status first.
 router.post('/login', login);
 router.post('/logout', logout);
+router.post('/register', register)
 
 module.exports = router;
