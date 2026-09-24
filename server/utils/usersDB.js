510e3b2bb847c6b96d88a05c8089de410251e1ef
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-24 14:23:31 +1200

    Fetcher and getter for users.

---
+++ server/utils/usersDB.js
@@ -0,0 +1,32 @@
+const pgp = require('pg-promise')();
+const connection = 'postgres://postgres:FishandChips69@localhost:5432/jiraiDB';
+const db = pgp(connection);
+
+async function dbGetUserByUsername(username){
+    try{
+        const userObject = await db.one(`SELECT * FROM testtable WHERE username = '${username}'`);
+        return userObject;
+    }
+    catch(e){
+        console.log("ERROR CAUGHT");
+        console.log(e);
+    }
+}
+
+async function dbAddUser(username, password){
+    try{
+        db.none(`INSERT INTO users(name, password) VALUES '${(username, password)}'`).then(() => {
+            console.log("User added successfully.");
+        })
+    }
+    catch(e){
+        console.log("ERROR CAUGHT");
+        console.log(e);
+    }
+}
+
+
+module.exports = {
+    dbGetUserByUsername,
+    dbAddUser,
+}
