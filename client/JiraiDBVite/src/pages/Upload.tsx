e75ff6068982a52a73e2f37b557ee8672ffee819
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-25 13:50:20 +1200

    upload form created. image gives a preview for the image uploaded.

--- client/JiraiDBVite/src/pages/Upload.tsx
+++ client/JiraiDBVite/src/pages/Upload.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+
 function Upload() {
+  //Upload image
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleUpload = (uploadEvent: React.ChangeEvent<HTMLInputElement>) => {
+    const file = uploadEvent.target.files?.[0];
+
+    if (!file) return;
+
+    setImageFile(file);
+    setPreview(URL.createObjectURL(file));
+  };
+
   return (
     <>
-      <p>UPLOAD PAGE</p>
+      <h1>UPLOAD</h1>
+      <form method="post">
+        <p>
+          <label>Title: </label>
+          <input type="text" name="username" id="title" />
+        </p>
+        <p>
+          <label>Description: </label>
+          <input type="text" name="username" id="description" />
+        </p>
+        <div>
+          <label>Category: </label>
+          <select>
+            <option value={"category1"}>category 1</option>
+            <option value={"category2"}>category 2</option>
+            <option value={"category3"}>category 3</option>
+          </select>
+        </div>
+        <div>
+          <label>Upload File: </label>
+          <input type="file" accept="image/*" onChange={handleUpload} />
+          <br />
+          {preview && <img src={preview} style={{ width: "300px" }} />}
+        </div>
+      </form>
     </>
   );
 }
