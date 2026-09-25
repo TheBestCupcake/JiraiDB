3b6b89f25a07f67bbbf86c17473a747d8dd90785
TheBestCupcake <cavaligrooby@gmail.com>
2026-09-25 14:11:57 +1200

    added upload route

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getItem, getAllOrSearchedItems} = require("../controllers/itemcontroller");
+const {getItem, getAllOrSearchedItems, uploadItem} = require("../controllers/itemcontroller");
 
 
 //router.get(Clothes/"path", response value);
 router.get("/", getAllOrSearchedItems);
 router.get("/:id", getItem);
+router.get("/upload", uploadItem);
 
 
 module.exports = router;
